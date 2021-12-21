1c1fd59557c945347a0e74ee0fb7677b32789686
Mental <Alekov99@mail.ru>
2021-12-21 15:38:57 +0500

    add router and  2 pages

--- src/App.vue
+++ src/App.vue
@@ -1,107 +1,32 @@
 <template>
-    <div id="app" class="main-page">
-
-        <div class="main-page__content-wrapper">
-
-            <div class="main-page__chessboard">
-                <chessboard @onMove="saveHistory"/>
-            </div>
-    
-            <div class="main-page__aside">
-
-                <h2 class="main-page__title">Ходы</h2>
-
-                <div ref="container" class="scrollable-container">
-
-                    <ul class="unstyled-list">
-                        <li v-for="item in movesHistory">
-                            {{ item.move }}
-                        </li>
-                    </ul>
-
-                </div>
-
-            </div>
-
-        </div> 
-
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Menu</router-link> |
+      <router-link to="/chess">Chess</router-link>
     </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import {chessboard} from 'vue-chessboard'
-import 'vue-chessboard/dist/vue-chessboard.css'
-import './styles/index.css'
-
-export default {
-    name: 'app',
-
-    components: {
-        chessboard,
-    },
-
-    data() {
-        return {
-            movesHistory: [],
-            cnt: 0,
-            turn: 'white',
-            pieces: {
-                white: {
-                    K: '♔',
-                    Q: '♕',
-                    R: '♖',
-                    B: '♗',
-                    N: '♘',
-                    P: '♙'
-                },
-                black: {
-                    K: '♚',
-                    Q: '♛',
-                    R: '♜',
-                    B: '♝',
-                    N: '♞',
-                    P: '♟'
-                },
-            },
-        }
-    },
-
-    methods: {
-        saveHistory(data) {
-            if (data.history.length === 0) {
-                return;
-            }
-            this.movesHistory.push(
-                {
-                    id: this.cnt++,
-                    move: this.transformToChessPiece(data.history[data.history.length - 1])
-                }
-            );
-            this.turn = data.turn;
-            setTimeout(this.scrollToEnd);
-        },
-
-        transformToChessPiece(item) {
-            switch (item[0]) {
-                case 'K':
-                case 'Q':
-                case 'R':
-                case 'B':
-                case 'N':
-                    item = this.pieces[this.turn][item[0]] + item.slice(1);
-                    break;
-                default:
-                    item = this.pieces[this.turn]['P'] + item;
-                    break;
-            }
-            return item;
-        },
-
-        scrollToEnd() {
-            let container = this.$refs.container;
-            container.scrollTop = container.scrollHeight;
-        },
-    },
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-</script>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
